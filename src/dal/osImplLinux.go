de6ee247e68f0ca90ee7be60556343fff85707e0
Kumar Karan <kumar.karan@continuum.net>
2017-02-28 20:11:07 +0530

    Changes for gometalinter failure RMM-13850

--- src/dal/osImplLinux.go
+++ src/dal/osImplLinux.go
@@ -3,7 +3,6 @@ package dal
 import (
 	"os/exec"
 	"strings"
-	"time"
 
 	amodel "github.com/ContinuumLLC/platform-api-model/clients/model/Golang/resourceModel/asset"
 )
@@ -38,14 +37,6 @@ func getOsInfo ()  *amodel.AssetOs {
     version := strings.Replace(string(out), "\n","",-1)
     version = strings.Replace(version, "\t","",-1)
     os.Version = version
-
-    //OsInstallDate
-    cmdName = "ls -ld /var/log/installer | cut -d \" \" -f6,7,8"
-    out, _ = exec.Command("bash", "-c", cmdName).Output()
-    installDateString := strings.Replace(string(out), "\n","",-1)
-    installDateString = strings.Replace(installDateString, "\t","",-1)
-    t, _ := time.Parse("Jan 13, at 12:05", installDateString)
-    os.InstallDate = t
 
     return os
 }
