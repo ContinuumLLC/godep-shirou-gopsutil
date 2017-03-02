50cca87a4b6573191a760af746cdf5164dc0a7ae
RajGupta-Continuum <rajkumar.gupta@continuum.net>
2017-03-02 14:32:55 +0530

    Fixing build warnings

--- src/dal/linux/bios.go
+++ src/dal/linux/bios.go
@@ -5,29 +5,27 @@ import (
 )
 
 func getBiosInfo() *asset.AssetBios {
+	//TODO : error handling
 	bios := new(asset.AssetBios)
 	util := dalUtil{}
 	//BiosManufacturer
 	cmdName := "sudo dmidecode -s bios-vendor"
-	manufacturer, err := util.execCommand(cmdName)
-	if err != nil {
-		//TODO : Add logging
-	}
+	manufacturer, _ := util.execCommand(cmdName)
 	bios.Manufacturer = manufacturer
 
 	//BiosSerialNumber
 	cmdName = "sudo dmidecode -s baseboard-serial-number"
-	serialNumber, err := util.execCommand(cmdName)
+	serialNumber, _ := util.execCommand(cmdName)
 	bios.SerialNumber = serialNumber
 
 	//BiosVersion
 	cmdName = "sudo dmidecode -s bios-version"
-	ver, err := util.execCommand(cmdName)
+	ver, _ := util.execCommand(cmdName)
 	bios.Version = ver
 
 	//SMBIOSVersion
 	cmdName = "sudo dmidecode --type bios | grep SMBIOS |  awk '{print $2}'"
-	smbiosVer, err := util.execCommand(cmdName)
+	smbiosVer, _ := util.execCommand(cmdName)
 	bios.SmbiosVersion = smbiosVer
 
 	return bios
