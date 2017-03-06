1e9435ad805de69d01257a40d09aa46d55618960
Praveen Upadhyay <praveen.upadhyay@continuum.net>
2017-03-06 14:45:38 +0530

    Added GetOS() unit test

--- src/dal/linux/linuxDal_test.go
+++ src/dal/linux/linuxDal_test.go
@@ -1,20 +1,52 @@
 package linux
 
 import (
+	"bytes"
+	"errors"
+	"io/ioutil"
 	"testing"
 
+	"strings"
+
+	"github.com/ContinuumLLC/platform-asset-plugin/src/model"
+	"github.com/ContinuumLLC/platform-asset-plugin/src/model/mock"
+	eMock "github.com/ContinuumLLC/platform-common-lib/src/env/mock"
+	"github.com/ContinuumLLC/platform-common-lib/src/logging"
+	"github.com/ContinuumLLC/platform-common-lib/src/procParser"
+	pMock "github.com/ContinuumLLC/platform-common-lib/src/procParser/mock"
 	"github.com/golang/mock/gomock"
 )
 
-func TestGetOS(t *testing.T) {
+func setupGetCommandReader(t *testing.T, parseErr error, commandReaderErr error) (*gomock.Controller, *mock.MockAssetDalDependencies) {
 	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	//mockAssetDalD := mock.NewMockAssetCollectionDalDependencies
+	mockAssetDalD := mock.NewMockAssetDalDependencies(ctrl)
 
-	// log := logging.GetLoggerFactory().New("")
-	// log.SetLogLevel(logging.OFF)
-	// _, err := AssetDalImpl{
-	// 	Factory: mockAssetDalD,
-	// 	Logger:  log,
-	// }.GetOS()
+	mockEnv := eMock.NewMockEnv(ctrl)
+	byteReader := bytes.NewReader([]byte("data"))
+	reader := ioutil.NopCloser(byteReader)
+	mockEnv.EXPECT().GetCommandReader(gomock.Any(), gomock.Any()).Return(reader, commandReaderErr)
+	mockAssetDalD.EXPECT().GetEnv().Return(mockEnv)
+
+	mockParser := pMock.NewMockParser(ctrl)
+	if commandReaderErr == nil {
+		mockParser.EXPECT().Parse(gomock.Any(), gomock.Any()).Return(&procParser.Data{}, parseErr)
+	}
+	mockAssetDalD.EXPECT().GetParser().Return(mockParser)
+
+	return ctrl, mockAssetDalD
+}
+
+func TestGetOS(t *testing.T) {
+	ctrl, mockAssetDalD := setupGetCommandReader(t, nil, errors.New(model.ErrExecuteCommandFailed))
+	defer ctrl.Finish()
+
+	log := logging.GetLoggerFactory().New("")
+	log.SetLogLevel(logging.OFF)
+	_, err := AssetDalImpl{
+		Factory: mockAssetDalD,
+		Logger:  log,
+	}.GetOS()
+	if err == nil || !strings.HasPrefix(err.Error(), model.ErrExecuteCommandFailed) {
+		t.Errorf("Expected error is %s, but received %v", model.ErrExecuteCommandFailed, err)
+	}
 }
