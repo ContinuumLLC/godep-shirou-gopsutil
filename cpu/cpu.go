a08b2bf7e7b538df848d6c3473dbb724edf88af6
Kedar Babar <kedar.babar@continuum.net>
2020-02-12 12:43:19 +0530

    [RMM-51756] : Updated init call

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -52,13 +52,18 @@ type lastPercent struct {
 var lastCPUPercent lastPercent
 var invoke common.Invoker
 
-func Init() {
+func init() {
 	invoke = common.Invoke{}
+	go getlastCPUPercent()
+}
+
+func getlastCPUPercent() {
 	lastCPUPercent.Lock()
 	lastCPUPercent.lastCPUTimes, _ = Times(false)
 	lastCPUPercent.lastPerCPUTimes, _ = Times(true)
 	lastCPUPercent.Unlock()
 }
+
 
 func Counts(logical bool) (int, error) {
 	return runtime.NumCPU(), nil
